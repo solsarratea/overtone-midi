8046b87f2fd05bddb46ab0cdf851c9ea35f4d3f5
sol <mssarratea@gmail.com>
2019-12-12 00:12:19 -0300

    Update piano midi player

--- src/overtone_midi/piano_midi.clj
+++ src/overtone_midi/piano_midi.clj
@@ -1,68 +1,8 @@
 (ns overtone-midi.piano-midi
-  (:require [overtone.midi.file :as midi]
+  (:require [overtone-midi.parser :as parser]
             [overtone.live      :as live]
             [overtone.inst.piano :refer [piano]]
-            [overtone.music.time :refer [apply-by now]]))
-
-(defn midi
-  "Reads MIDI from file-path"
-
-  [name]
-  (midi/midi-file (str "resources/midis/" name ".mid")))
-
-(defn- find-note-off
-  "Returns true if midi-event is the note-off of input note"
-
-  [original-note {:keys [note command]}]
-  (every?  true? [(= note original-note) (= command :note-off)]))
-
-(defn- parse-event
-  "Select from midi-event note, velocity and timestamp.
-   Add duration between note-on and note-off events."
-
-  [{:keys [note timestamp] :as event} events]
-  (let [event-off (first (filter #(find-note-off note %) events))]
-    (when (not (nil? event-off))
-        (let [duration  (- (:timestamp event-off) timestamp)]
-        {:event     (-> event
-                        (select-keys [:note :velocity :timestamp])
-                        (assoc :duration duration))
-        :note-off (vec (remove #(= event-off %) events))}))))
-
-(defn- select-and-parse-events
-  "Create only sequence of parsed midi-event from note-on and note-off events."
-
-  [{:keys [note-on note-off]}]
-  (let [filtered-note-on (vec (remove #(zero? (:velocity %)) note-on))]
-    (second
-     (reduce
-     (fn [[note-off-events acum] event]
-       (let [{:keys [event note-off] :as data} (parse-event event note-off-events)]
-         (if (nil? data)
-           [note-off-events  acum]
-           [note-off         (vec (conj acum event))]
-           )))
-     [note-off []]
-     filtered-note-on))))
-
-(defn- compases
- "Group events according to their timestamp."
-
- [events]
- (->> events
-      (group-by :timestamp )
-      (sort-by first)))
-
-
-(defn parse-midi
-  "Returns sequence of parsed midi-events grouped by timestamp.
-   It assumes midi has only channel."
-
- [midi]
- (let [midi-events   (-> midi :tracks first :events)
-       parsed-events (select-and-parse-events (group-by :command midi-events))]
-   (compases parsed-events)))
-
+            [overtone.music.time :refer [now]]))
 
 (defn- live-piano
   "Plays event on piano at specified time. "
@@ -82,17 +22,24 @@
   ([events tempo]
   (loop [ time        (now)
          [t0 actuals] (first events)
-         ev-rest         (rest events)]
-    (let [[t1 _ :as next]   (first ev-rest)
+         ev-rest      (rest events)]
+    (when-let [first (first ev-rest)]
+      (let [[t1 _ :as next]   first
          in-between-time    (* tempo (- t1 t0))
           next-time           (+ time in-between-time)]
-      (when (not (nil? actuals))
+      (when-not (nil? actuals)
         (do
           (doall
             (map #(live-piano % time) actuals))
-          (recur next-time next (rest ev-rest))))))))
+          (recur next-time next (rest ev-rest)))))
+      )
+    )))
 
 
 (comment
-  (def midif (midi "love-dream"))
-  (play-on-piano (parse-midi midif))))
+  (play-on-piano (parser/read-and-parse-midi "love-dream")))
+
+
+(play-on-piano (parser/read-and-parse-midi "Tian"  ) 8)
+
+(live/stop)
